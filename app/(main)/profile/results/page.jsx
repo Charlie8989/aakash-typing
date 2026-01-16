cd88bf706fe5abf074180d9c48ec14b7b5248c0b
Charlie <charlie962873@gmail.com>
2026-01-16 20:55:01 +0530

    responsiveness increased

--- app/(main)/profile/results/page.jsx
+++ app/(main)/profile/results/page.jsx
@@ -32,7 +32,7 @@ const Page = () => {
     );
 
   return (
-    <div className="max-w-6xl h-[90vh] mx-auto p-6">
+    <div className="max-w-6xl min-h-[90vh] mx-auto p-6">
       <h1 className="text-2xl font-bold mb-6">Your Test History</h1>
 
       <div className="grid grid-cols-1 gap-5">
